afefd354241ebc8095522090f89a547219598392
MarcelaDJ <analista.desarrollo@spia.com.co>
2025-10-03 17:07:08 -0500

    try to run app id

---
+++ app/src/main/java/com/hoshiengineering/readerAssistant/MyReaderAssistant.kt
@@ -0,0 +1,56 @@
+package com.hoshiengineering.readerAssistant
+
+import android.app.Application
+import android.text.TextUtils
+import android.util.Log
+import com.hoshiengineering.core.utils.ServiceAvailabilityProvider
+import com.huawei.agconnect.AGConnectInstance
+import com.huawei.agconnect.AGConnectOptionsBuilder
+import com.huawei.hms.aaid.HmsInstanceId
+import com.huawei.hms.common.ApiException
+import com.huawei.hms.mlsdk.common.MLApplication
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyReaderAssistant: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val isHmsAvailable = ServiceAvailabilityProvider.isHmsAvailable(this)
+
+         if (isHmsAvailable) {
+
+             /*MLApplication.getInstance().apiKey =
+                 "DgEDALcjrS3ZR+VjwPA4Ut4BuP/npSkcw4fJsF/rLvgW6s0bAFQ/L2xjn+yYSOIh2MMoW38FOOD1oLNBV45in7lPXvz05FY0bFmYnQ=="*/
+             token
+        } else {
+             /* FirebaseMessaging.getInstance().token.addOnSuccessListener { result ->
+               LogTaxisLibres.i(TAG, "FCM token: $result")
+               tokenFcm = result
+           }*/
+        }
+    }
+
+    /* get hms token from huawei */
+    private val token: Unit
+        get() {
+            object : Thread() {
+                override fun run() {
+                    try {
+                        // read from agconnect-services.json
+                        val appId = AGConnectOptionsBuilder().build(applicationContext)
+                            .getString("client/app_id")
+                        val token =
+                            HmsInstanceId.getInstance(applicationContext).getToken(appId, "HCM")
+                        if (!TextUtils.isEmpty(token)) {
+                            Log.i("TAG", "HMS token: $token")
+                          //  tokenHcm = token
+                        }
+                    } catch (e: ApiException) {
+                        Log.e("TAG", "get token failed, $e")
+                    }
+                }
+            }.start()
+        }
+
+}
