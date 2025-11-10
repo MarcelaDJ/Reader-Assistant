0060e38783f0a637f6893e13386b1cb368eee955
MarcelaDJ <analista.desarrollo@spia.com.co>
2025-11-10 10:30:48 -0500

    add more dependeencies ,update engine config in tts add plugin agcp in core and app and level project

--- tts/src/main/java/com/hoshiengineering/tts/HuaweiTextToSpeechImpl.kt
+++ tts/src/main/java/com/hoshiengineering/tts/HuaweiTextToSpeechImpl.kt
@@ -4,13 +4,16 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.util.Pair
+import com.huawei.hms.mlsdk.model.download.MLLocalModelManager
 import com.huawei.hms.mlsdk.tts.MLTtsAudioFragment
 import com.huawei.hms.mlsdk.tts.MLTtsCallback
 import com.huawei.hms.mlsdk.tts.MLTtsConfig
-import com.huawei.hms.mlsdk.tts.MLTtsEngine
 import com.huawei.hms.mlsdk.tts.MLTtsConstants
+import com.huawei.hms.mlsdk.tts.MLTtsEngine
 import com.huawei.hms.mlsdk.tts.MLTtsError
+import com.huawei.hms.mlsdk.tts.MLTtsLocalModel
 import com.huawei.hms.mlsdk.tts.MLTtsWarn
+
 
 class HuaweiTextToSpeechImpl(context: Context) : AppTextToSpeech {
     private var engine: MLTtsEngine
@@ -22,6 +25,7 @@ class HuaweiTextToSpeechImpl(context: Context) : AppTextToSpeech {
             .setSpeed(1.0f)
             .setVolume(10.0f)
         engine = MLTtsEngine(config)
+        engine.updateConfig(config)
 
         engine.setTtsCallback(object : MLTtsCallback {
 
